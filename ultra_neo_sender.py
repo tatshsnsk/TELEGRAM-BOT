60288dad80aba946babb7a45f6035a177ffc892e
tatshsnsk <cybercctv90@gmail.com>
2025-12-02 04:06:08 +0600

    Delete ultra_neo_sender.py

--- ultra_neo_sender.py
+++
@@ -1,8 +0,0 @@
-import ultra_neo_sender
-
-# Inject BOT_TOKEN & API
-ultra_neo_sender.BOT_TOKEN = "8330501816:AAHsNLzOevZcSQZ6T1OyZ9TUjFf9B-Cq7gc"
-ultra_neo_sender.API = f"https://api.telegram.org/bot{ultra_neo_sender.BOT_TOKEN}/sendMessage"
-
-# Run the sender
-ultra_neo_sender.main()
