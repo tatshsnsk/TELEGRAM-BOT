f3b1ba7e5d5d9bfd5cfff577f3aabadd3b65d8b9
tatshsnsk <cybercctv90@gmail.com>
2025-12-06 23:22:48 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
-import tgvip  # NOT ultra_neo_sender
+# bot.py
+TOKEN = "8330501816:AAHsNLzOevZcSQZ6T1OyZ9TUjFf9B-Cq7gc"
 
-# Inject BOT_TOKEN & API
-tgvip.BOT_TOKEN = "8330501816:AAHsNLzOevZcSQZ6T1OyZ9TUjFf9B-Cq7gc"
-tgvip.API = f"https://api.telegram.org/bot{tgvip.BOT_TOKEN}/sendMessage"
+open("token.txt", "w").write(TOKEN)
 
-# Run the sender
+import tgvip   # your .so module name
 tgvip.main()
